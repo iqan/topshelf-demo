e5e242ee35c77c5ed4b5834afa757b02f2d9f907
iqans <iqan.shaikh@gmail.com>
2017-12-17 12:05:28 +0530

    init simple topshelf implementation

---
+++ TopshelfDemo/Processor.cs
@@ -0,0 +1,17 @@
+﻿namespace TopshelfDemo
+{
+    using System;
+    using System.Timers;
+
+    public class Processor
+    {
+        readonly Timer _timer;
+        public Processor(double interval)
+        {
+            _timer = new Timer(interval) { AutoReset = true };
+            _timer.Elapsed += (sender, eventArgs) => Console.WriteLine("It is {0} and all is well", DateTime.Now);
+        }
+        public void Start() { _timer.Start(); }
+        public void Stop() { _timer.Stop(); }
+    }
+}
---
+++ TopshelfDemo/Program.cs
@@ -0,0 +1,27 @@
+﻿using Topshelf;
+
+namespace TopshelfDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var intervalInSeconds = 1;
+
+            HostFactory.Run(x =>
+            {
+                x.Service<Processor>(s =>
+                {
+                    s.ConstructUsing(name => new Processor(intervalInSeconds * 1000));
+                    s.WhenStarted(tc => tc.Start());
+                    s.WhenStopped(tc => tc.Stop());
+                });
+                x.RunAsLocalSystem();
+
+                x.SetDescription("Topshelf Demo Service");
+                x.SetDisplayName("Topshelf Demo");
+                x.SetServiceName("Topshelf Demo");
+            });
+        }
+    }
+}
