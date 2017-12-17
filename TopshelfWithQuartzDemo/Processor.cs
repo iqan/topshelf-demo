4ff736aaec273226495557108a64694587d69b18
iqans <iqan.shaikh@gmail.com>
2017-12-17 12:44:08 +0530

    Topshelf with Quartz interation done

---
+++ TopshelfWithQuartzDemo/Processor.cs
@@ -0,0 +1,16 @@
+﻿namespace TopshelfWithQuartzDemo
+{
+    using Quartz;
+    using System;
+
+    public class Processor : IJob
+    {
+        public void Start() { Console.WriteLine("Topshelf.Quartz: Starting ", DateTime.Now); }
+        public void Stop() { Console.WriteLine("Topshelf.Quartz: Stopping ", DateTime.Now); }
+
+        public void Execute(IJobExecutionContext context)
+        {
+            Console.WriteLine("Topshelf.Quartz: It is {0} and all is well", DateTime.Now);
+        }
+    }
+}
---
+++ TopshelfWithQuartzDemo/Program.cs
@@ -0,0 +1,41 @@
+﻿namespace TopshelfWithQuartzDemo
+{
+    using System;
+    using Quartz;
+    using Topshelf;
+    using Topshelf.Quartz;
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var intervalInSeconds = 1;
+
+            HostFactory.Run(x =>
+            {
+                x.Service<Processor>(s =>
+                {
+                    s.ConstructUsing(name => new Processor());
+                    s.WhenStarted(tc => tc.Start());
+                    s.WhenStopped(tc => tc.Stop());
+
+                    s.ScheduleQuartzJob(q =>
+                        q.WithJob(() =>
+                            JobBuilder.Create<Processor>().Build())
+                        .AddTrigger(() =>
+                            TriggerBuilder.Create()
+                                .WithSimpleSchedule(builder => builder
+                                    .WithIntervalInSeconds(intervalInSeconds)
+                                    .RepeatForever())
+                                .Build())
+                        );
+                });
+                x.RunAsLocalSystem();
+
+                x.SetDescription("Topshelf Demo Service");
+                x.SetDisplayName("Topshelf Demo");
+                x.SetServiceName("Topshelf Demo");
+            });
+        }
+    }
+}
